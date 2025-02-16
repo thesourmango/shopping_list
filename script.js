17056d991f512f08c8663ba7c6a69459a2feb24d
thesourmango <juhuuuuu@hotmail.com>
2025-02-16 15:22:49 +0200

    Added localstorage support

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 /* Global item counter and item_values */
 let item_counter = 2
 
+/* Check if theres a list in localstorage */
+const saved_list = localStorage.getItem('item_list');
+if (saved_list !== null && JSON.parse(saved_list).length !== 0 && saved_list !== "false") {
+    console.log('Found saved list: ', JSON.parse(saved_list));
+    const saved_array = JSON.parse(saved_list)
+    // Reset list and reload from localstorage
+    item_counter = 0
+    document.querySelector("section").innerHTML = '<input type="text" id="new_item">'
+    // Add all list elements from localstorage to DOM
+    saved_array.forEach(list_item => {
+        add_item(list_item)
+    });
+}
+
+
+
 /* Trashcan functionality */
 function remove_item(element) {
     // console.log(element.target.parentElement) // debug
@@ -20,11 +36,13 @@ function item_up(element) {
     let upperSibling = element.target.parentElement.previousElementSibling;
     if(upperSibling === null) return;
     upperSibling.insertAdjacentElement("beforebegin", element.target.parentElement);
+    track_list_items()
 }
 function item_down(element) {
     let lowerSibling = element.target.parentElement.nextElementSibling;
     if(lowerSibling === null) return;
     lowerSibling.insertAdjacentElement("afterend", element.target.parentElement);
+    track_list_items()
 }
 
 function attach_updown_listeners() {
@@ -44,8 +62,10 @@ var checkbox = document.querySelector("input[type=checkbox]")
 function check_item(e) {
   if (e.target.checked) {
     console.log("Checked for " + e.target.value)
+    track_list_items()
   } else {
     console.log("Unckecked for "+e.target.value)
+    track_list_items()
   }
 }
 
@@ -76,7 +96,35 @@ function add_item(e) {
         attach_trash_listeners()
         attach_updown_listeners()
         attach_check_listeners()
+        track_list_items()
         e.target.value = ""
+    }
+    // When fetching list from localstorage
+    else if (e.key == undefined) {
+        // console.log(e) // debug
+        item_counter++
+        // If saved list item has been checked
+        if (e.checked == true) {
+            row.innerHTML = '<input type="checkbox" id="' + item_counter + '" value="' + e.item + '" checked><label for="' + item_counter + '">' + e.item + '</label><span class="up">⬆️</span><span class="down">⬇️</span><span class="trash">❎</span><br>'
+        }
+        else {
+            row.innerHTML = '<input type="checkbox" id="' + item_counter + '" value="' + e.item + '"><label for="' + item_counter + '">' + e.item + '</label><span class="up">⬆️</span><span class="down">⬇️</span><span class="trash">❎</span><br>'
+        }
+        document.querySelector("section").insertBefore(row,document.querySelector("#new_item"))
+        attach_trash_listeners()
+        attach_updown_listeners()
     }
 }
 document.querySelector("#new_item").addEventListener("keypress", add_item)
+
+
+/* State management for localstorage */
+function track_list_items(){
+    let item_values = [];
+    let items = document.querySelectorAll('input:not([type="text"])')
+    items.forEach(item => {
+        item_values.push({"item":item.value, "number":item.id, "checked":item.checked})
+    })
+    //console.log(item_values) // debug, displays list before sending update to DB
+    localStorage.setItem('item_list', JSON.stringify(item_values))
+}
